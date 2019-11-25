258734cccf42e1418e6f55e9e05e3b1303cca5a5
Wilfred <taghawilfred@gmail.com>
2019-11-25 14:25:03 +0100

    print stement added

--- bikeshare.py
+++ bikeshare.py
@@ -18,6 +18,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
+    print('Hello, Hello, Hello, \n This is the bikeshare project. Are you ready to go if yes then\n')
     print('Hello! Let\'s explore some US bikeshare data!')
     print('Please all the information you type in should be in lower case else you will be asked to enter a valid one.')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inpts
